3f7d765fc7c7d61bff16e4cd7dbded2c2287413f
cristinam <mateescu.cristina24@yahoo.ro>
2019-06-23 14:07:32 +0300

    Initial commit

--- src/test/java/com/company/ArrayUtilsTest.java
+++ src/test/java/com/company/ArrayUtilsTest.java
@@ -1,13 +1,22 @@
-import com.company.ArraysUtils;
+package com.company;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArrayUtilsTest {
+    private ArraysUtils arrayUtils;
+
+    @Before
+    public void SetUp() {
+        arrayUtils=new ArraysUtils();
+    }
+
     @Test
     public void binarySearch_elementExist() {
         int[] array = {1, 3, 5, 7, 9, 10};
         int n = 7;
-        int result = new ArraysUtils().binarySearch(array, n);
+        int result = arrayUtils.binarySearch(array, n);
         Assert.assertEquals(3, result);
 
 
@@ -17,7 +26,7 @@ public class ArrayUtilsTest {
     public void binarySearch_elementNotExist() {
         int[] array = {1, 3, 5, 7, 9, 10};
         int n = 2;
-        int result = new ArraysUtils().binarySearch(array, n);
+        int result = arrayUtils.binarySearch(array, n);
         Assert.assertEquals(-2, result);
     }
     @Test
@@ -25,7 +34,7 @@ public class ArrayUtilsTest {
     {
         int[] array = {1, 5, 7,3, 9, 10};
         new ArraysUtils().bubbleSort(array);
-
+        arrayUtils.bubbleSort(array);
         Assert.assertArrayEquals(new int[]{1, 3, 5,7, 9, 10},array);
     }
 }
